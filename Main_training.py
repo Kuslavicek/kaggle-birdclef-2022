d888261e5c548b1b8f9f94f26ed1a626b6adc75e
Kuslavicek <38736526+Kuslavicek@users.noreply.github.com>
2022-03-23 12:44:57 +0100

    first commit

---
+++ Main_training.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import soundfile as sf
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Input, Dropout, TimeDistributed, Flatten
+import matplotlib.pyplot as plt
+from  tensorflow.keras.applications.vgg16 import VGG16
+
+
+train_df = pd.read_csv("D:\\Programovani\\Workspace 2.0\\birdclef-2022\\df2.csv")
+birds_id = pd.read_csv("D:\\Programovani\\Workspace 2.0\\birdclef-2022\\birds_id.csv")
+train_df.sample(frac=1).reset_index(drop=True)
+valid_df = train_df[10000:].reset_index(drop=True)
+train_df = train_df[:10000]
+print(train_df.shape)
+print(valid_df.head())
+
+
+class VoiceGenerator(tf.keras.utils.Sequence):
+    def __init__(self,df,id_list):
+        self.df = df
+        self.id_list = id_list
+    def __getitem__(self,index):
+        X = self.__generate_X(index)
+        y = self.__generate_y(index)
+        return X, y
+    def __len__(self):
+        return int(len(self.df))
+    def __generate_X(self,index):
+        row = self.df.loc[index,:]
+        file = "train_spec_128/"+row['filename'][:-4]+".npy"
+        array = np.load(file)
+        return np.resize(array,(1,128,128,1))        
+    def __generate_y(self,index):
+        array = np.zeros(21,np.int8)
+        row = self.df.loc[index,:]
+        id = row['primary_label']
+        number = self.id_list.loc[self.id_list['string']==id,'index']
+        array[number]=1
+        return np.resize(array,(1,21))
+    
+    
+    
+def build_conv():
+    conv_model = keras.Sequential()
+    conv_model.add(Conv2D(64,1,1,activation='relu'))
+    conv_model.add(Conv2D(64,1,1,activation='relu'))
+    conv_model.add(Conv2D(64,1,1,activation='relu'))
+    conv_model.add(MaxPooling2D(2,2))
+    conv_model.add(Conv2D(128,2,activation='relu'))
+    conv_model.add(Conv2D(128,2,activation='relu'))
+    conv_model.add(Conv2D(128,2,activation='relu'))
+    conv_model.add(MaxPooling2D(2,2))
+    conv_model.add(Dropout(0.5))
+    conv_model.add(Dense(64, activation='relu'))
+    conv_model.add(Dense(64, activation='relu'))
+    conv_model.add(Dense(64, activation='relu'))
+    conv_model.add(Flatten())
+    conv_model.add(Dense(21,activation='softmax'))
+    conv_model.compile(optimizer = 'Adam',loss= 'categorical_crossentropy',metrics = ['accuracy'] )
+    return conv_model
+
+
+
+
+model = VGG16(classes=21,weights=None, input_shape=(128,128,1))
+model.compile(optimizer = 'Adam',loss= 'categorical_crossentropy',metrics = ['accuracy'] )
+history=model.fit(VoiceGenerator(train_df,birds_id),epochs=3)
+model.evaluate(VoiceGenerator(valid_df,birds_id))
+model.summary()
+
---
+++ Reshape.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import soundfile as sf
+import matplotlib.pyplot as plt
+import os
+import gc
+
+gc.enable()
+df = pd.read_csv("df2.csv")
+for i, row in df.iterrows():
+    path = row['filename']
+    try:
+        sound= np.zeros((32000*6))
+        sf.read("train_audio/"+path,out=sound)
+        spectrogram , l ,g,r = plt.specgram(sound,Fs=32000)
+    except:    
+        sound= np.zeros((32000*6,2))
+        sf.read("train_audio/"+path,out=sound)
+        spectrogram , l ,g,r = plt.specgram(sound[:,1],Fs=32000)
+    del sound
+    del l,g,r
+    gc.collect()
+    spectrogram = np.resize(spectrogram,(1,128,128,1)).astype(np.float32)
+    patharr = path.split("/")
+    if(os.path.exists("train_spec_128/"+patharr[0])!=True):
+        os.mkdir("train_spec_128/"+patharr[0])
+    np.save("D:\\Programovani\\Workspace 2.0\\birdclef-2022\\train_spec_128\\"+patharr[0]+"\\"+patharr[1][:-4]+".npy",spectrogram)
+    del spectrogram
+    print("Completed " +str(i)+" of "+str(len(df)))
